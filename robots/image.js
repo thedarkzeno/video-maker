6f0520130308ea75365e59aa9d54156c2e2ff8f4
Adalberto <multiolx@gmail.com>
2019-04-22 14:15:44 -0300

    download images

--- robots/image.js
+++ robots/image.js
@@ -1,43 +1,71 @@
-const google=require("googleapis").google
-const customSearch=google.customsearch("v1")
-const state=require("./state.js")
+const imageDownloader = require('image-downloader')
+const google = require("googleapis").google;
+const customSearch = google.customsearch("v1");
+const state = require("./state.js");
 
-const googleSearchCredentials=require("../credentials/google-search.json")
+const googleSearchCredentials = require("../credentials/google-search.json");
 
-async function robot(){
-    const content=state.load()
-    // const imagesArray = await fetchGoogleAndReturnImagesLinks("Michael Jackson")
-    // console.dir(imagesArray, {depth: null})
-    // process.exit(0)
-    await fetchImagesOfAllSentences(content)
-    state.save(content)
-    async function fetchImagesOfAllSentences(content){
-        for(const sentence of content.sentences){
-            const query = `${content.searchTerm} ${sentence.keywords[0]}`
-            sentence.images = await fetchGoogleAndReturnImagesLinks(query)
+async function robot() {
+  const content = state.load();
+  // const imagesArray = await fetchGoogleAndReturnImagesLinks("Michael Jackson")
+  // console.dir(imagesArray, {depth: null})
+  // process.exit(0)
+  await fetchImagesOfAllSentences(content)
+  await downloadAllImages(content);
+  state.save(content)
+  async function fetchImagesOfAllSentences(content) {
+    for (const sentence of content.sentences) {
+      const query = `${content.searchTerm} ${sentence.keywords[0]}`;
+      sentence.images = await fetchGoogleAndReturnImagesLinks(query);
 
-            sentence.googleSearchQuery = query
-        }
+      sentence.googleSearchQuery = query;
     }
+  }
 
+  async function fetchGoogleAndReturnImagesLinks(query) {
+    const response = await customSearch.cse.list({
+      auth: googleSearchCredentials.apiKey,
+      cx: googleSearchCredentials.searchEngineId,
+      q: query,
+      num: 2,
+      searchType: "image",
+    //   imgSize: "huge"
+    });
+    const imagesUrl = response.data.items.map(item => {
+      return item.link;
+    });
+    return imagesUrl;
+  }
 
-    async function fetchGoogleAndReturnImagesLinks(query){
-        const response = await customSearch.cse.list({
-            auth: googleSearchCredentials.apiKey,
-            cx: googleSearchCredentials.searchEngineId,
-            q: query,
-            num: 2,
-            searchType:'image',
-            imgSize: 'huge'
-        })
-        const imagesUrl = response.data.items.map((item)=>{
-            return item.link
-        })
-        return imagesUrl
-    }
+  async function downloadAllImages(content){
+      content.downloadedImages=[]
+      
+      for(let sentenceIndex =0; sentenceIndex<content.sentences.length;sentenceIndex++){
+          const images = content.sentences[sentenceIndex].images
 
-    
-    
-    
+          for(let imageIndex=0; imageIndex<images.length; imageIndex++){
+              const imageUrl = images[imageIndex]
+
+              try{
+                  if(content.downloadedImages.includes(imageUrl)){
+                      throw new Error('Imagem já foi baixada')
+                  }
+                  await downloadAndSave(imageUrl, `${sentenceIndex}-original.png`) 
+                 content.downloadedImages.push(imageUrl)
+                 console.log(`> baixou imagem com sucesso: ${imageUrl}`)
+                 break
+                }catch(error){
+                  console.log("> erro ao baixar imagem")
+              }
+          }
+      }
+  }
+  async function downloadAndSave(url, fileName){
+      return imageDownloader.image({
+          url: url,
+          dest: `./content/${fileName}`
+      })
+  }
+
 }
-module.exports=robot
+module.exports = robot;
